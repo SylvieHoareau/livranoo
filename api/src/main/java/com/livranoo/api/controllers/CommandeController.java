5d575dd9bb2ae8a6be5ac787c0a8c3d1e56c72be
hoareausylvie <hoareauylvie@outlook.fr>
2026-04-21 16:22:57 +0400

    feat: ajout d'un controller et d'un model

--- api/src/main/java/com/livranoo/api/controllers/CommandeController.java
+++ api/src/main/java/com/livranoo/api/controllers/CommandeController.java
@@ -1,5 +1,29 @@
 package com.livranoo.api.controllers;
 
+import com.livranoo.api.models.Commande;
+import com.livranoo.api.repositories.CommandeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/commandes")
+@CrossOrigin(origins = "http://localhost:4200") // Autorise Angular à appeler l'API
 public class CommandeController {
-    
+
+    @Autowired
+    private CommandeRepository commandeRepository;
+
+    // Récupérer tous les colis
+    @GetMapping
+    public List<Commande> getAllCommandes() {
+        return commandeRepository.findAll();
+    }
+
+    // Ajouter un nouveau colis (utile pour tes tests)
+    @PostMapping
+    public Commande createCommande(@RequestBody Commande commande) {
+        return commandeRepository.save(commande);
+    }
 }
